3756b88a7ecb99cd28fbc692377c763827e60547
R morel <38894494+mrl893@users.noreply.github.com>
2021-12-08 10:08:21 -0500

    Add files via upload

---
+++ car_in_constructors.js
@@ -0,0 +1,17 @@
+function  Car(model, year, miles) {
+  this.model = model;
+  this.year = year;
+  this.miles = miles;
+
+
+  this.toString = function(){
+    return this.model + " has done " + this.miles + " miles";
+  };
+
+}
+
+var civic  = new Car("Honda Civic", 2009, 2000);
+var mondeo = new Car("ford Mondeo", 2010, 5000)
+
+console.log(civic.toString());
+console.log(mondeo.toString());
